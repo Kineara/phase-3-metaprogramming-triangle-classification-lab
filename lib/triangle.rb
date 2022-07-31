bb4574f05ef73c983ce80bde53ac651f6a319a31
Lauren Hoock <laurenh8916@protonmail.com>
2022-07-30 21:42:18 -0600

    completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_reader :sides
+
+  def initialize(side_one, side_two, side_three)
+    # @side_one = side_one
+    # @side_two = side_two
+    # @side_three = side_three
+    @sides = [side_one, side_two, side_three]
+  end
+
+  def kind 
+    if self.sides.min > 0 && self.sides[0] + self.sides[1] > self.sides[2] && self.sides[0] + self.sides[2] > self.sides[1] && self.sides[1] + self.sides[2] > self.sides[0]
+      case self.sides.uniq.length
+      when 1
+        :equilateral
+      when 2
+        :isosceles
+      when 3
+        :scalene
+      end
+    else
+        raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
